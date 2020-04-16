b02e00f3eb4ab6f49739994cdce2ed10b62e8bc2
tostos <saittosun2@gmail.com>
2020-04-16 08:21:11 +0200

    connecting the store to react

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,7 +1,9 @@
+// jshint esversion: 6
 import React, { Component } from 'react';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import { connect } from 'react-redux';
 
 class Counter extends Component {
     state = {
@@ -22,13 +24,15 @@ class Counter extends Component {
             case 'sub':
                 this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
                 break;
+            default:
         }
     }
 
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                {/* <CounterOutput value={this.state.counter} /> */}
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
@@ -38,4 +42,13 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+// the name is totally up to you but it's very clear about what you will store in here. You store instructions about how the state managed by redux should be mapped to props you can use in this container because that's important, the state managed redux is not received as state here because state is the thing you change internally from within a component. Those times are over, redux is now the place where we manage and change the state, so we don't want to get anything which we can change internally and props are changed internally, that is why we map the redux state to props. This is where the name comes from,
+const mapStateToProps = state => {// the state stored in redux as the     input and returns a javascript object which is a map of prop names and slices of the state stored in redux.
+    return {
+        // this state here again, will be given to you by react-redux which of course will reach out to your redux state which of course in turn is the state you set up here, so there will be a counter property available.
+        ctr: state.counter
+    }
+
+}
+
+export default connect(mapStateToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './store/reducer';
+import { Provider } from 'react-redux';
 
 // This store should be created right before our application or when our application starts, so the index.js file is a great place, this is where we mount our app component to the dom, so creating the store here also makes a lot of sense.
 // With that we're creating a store successfully with our own reducer, just like we learned before in the nodeJS file.
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // Provider is a helper component which allows us to kind of inject our store into the react components.
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
