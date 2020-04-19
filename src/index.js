5b2140cf85ba9f9f2839022a997755e48893b42f
tostos <saittosun2@gmail.com>
2020-04-19 08:22:54 +0200

    adding middelware

--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import { Provider } from 'react-redux';
+
+//this will be the name of my middleware. It will get the store as an input, this is the case because we will soon use a specific method provided by redux to connect our own middleware to the store and this method provided by redux will eventually execute our middleware function and give us the store.
+const logger = store => {
+  // you can name this argument whatever you want but next makes sense because this will be a function which you can execute to let the action continue its journey onto the reducer,
+  return next => {
+    // this function now also returns a function, which will receive the action you dispatched as an input. So this nested function party here is simply a middleware, now inside that inner function which receives the action, we can also access a store and this next function and of course the action itself.
+    return action => {
+      console.log('[middelware] dispatching', action);
+      // this will now let the action continue to the reducer, though for that to succeed, we need to pass the action as an argument, now that's important because you could theoretically also change that action here in the middleware, we have access to it, we get it as an argument, we could change the type. 
+      const result = next(action);
+      console.log('[middelware] next state', store.getState());
+      return result;
+      //  this function tree is in the end what gets executed, all of that is done by redux, we don't have to call any of these functions, all we have to do is apply this middleware to our store.
+    };
+  };
+};
 
 // this is a function which takes a javascript object mapping our reducers to different slices of our state as input and merges everything into one state and one reducer for us.
 const rootReducer = combineReducers({
@@ -18,7 +34,8 @@ const rootReducer = combineReducers({
 
 // This store should be created right before our application or when our application starts, so the index.js file is a great place, this is where we mount our app component to the dom, so creating the store here also makes a lot of sense.
 // With that we're creating a store successfully with our own reducer, just like we learned before in the nodeJS file.
-const store = createStore(rootReducer);
+// this function as the names suggests allows us to add our own middleware to the store.
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   // Provider is a helper component which allows us to kind of inject our store into the react components.
